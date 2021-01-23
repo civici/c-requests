de0cfd3613d342f32ba9ddc2888aa92c8e9c851a
civici <huseyincivici@outlook.com>
2021-01-23 21:46:42 +0300

    test

--- build.bat
+++
@@ -1,5 +0,0 @@
-set sources=main.c sockr.c
-set includepath=C:\vcpkg/installed/x64-windows/include
-set libpath=C:\vcpkg/installed/x64-windows/lib
-
-cl /EHsc /MD /I%includepath% %sources% /link /LIBPATH:%libpath% /out:a.exe
